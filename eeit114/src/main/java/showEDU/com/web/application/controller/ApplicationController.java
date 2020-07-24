050fc19911d570667ccd0bc1f4c95e1f2f648d20
貓豬 <@DESKTOP-OKCJA8F>
2020-07-24 09:31:31 +0800

    application修改

--- eeit114/src/main/java/showEDU/com/web/application/controller/ApplicationController.java
+++ eeit114/src/main/java/showEDU/com/web/application/controller/ApplicationController.java
@@ -32,50 +32,49 @@ import showEDU.com.web.application.service.ApplicationService;
 
 import showEDU.com.web.member.model.MemberBean;
 
-
-
-
 @Controller
-@SessionAttributes({"memberBean"})
+@SessionAttributes({ "memberBean" })
 public class ApplicationController {
 	@Autowired
 	ApplicationService aplcService;
 	@Autowired
 	ServletContext ctx;
-	
-	//算總頁數
-		public Integer getTotalpage(int listLength) {
-			if((listLength % 2) == 0) {
-				return (listLength/2);
-			}else {
-				return (listLength/2)+1;
-			}
+
+	// 算總頁數
+	public Integer getTotalpage(int listLength) {
+		if ((listLength % 2) == 0) {
+			return (listLength / 2);
+		} else {
+			return (listLength / 2) + 1;
 		}
-	
-	
+	}
+
+	// 申請租借，產生表單(客戶端)
 	@GetMapping("/application/add")
 	public String getAddNewApplicationForm(Model model) {
-		ApplicationBean aplcBean =new ApplicationBean();
+		ApplicationBean aplcBean = new ApplicationBean();
 		model.addAttribute("aplcBean", aplcBean);
 		MemberBean memberBean = (MemberBean) model.getAttribute("memberBean");
 		if (memberBean == null) {
 			return "redirect: " + ctx.getContextPath() + "/member/crm/login";
 		}
-		return 	"application/addApplication";
+		return "application/addApplication";
 	}
-	
+
+	// 申請租借，產生包含選擇日期且狀態為未被借出之Bean
+	// 用途為將可選的時段放入下拉選單(客戶端)
 	@GetMapping("/getOrderableTime")
-	public ResponseEntity<List<String>> getOrderableTime
-	(@RequestParam(value="selectDate", defaultValue = "0") Date selectDate)  {
+	public ResponseEntity<List<String>> getOrderableTime(
+			@RequestParam(value = "selectDate", defaultValue = "0") Date selectDate) {
 		List<String> list = aplcService.getOrderableTime(selectDate);
-		ResponseEntity<List<String>> re = new ResponseEntity<>(list,HttpStatus.OK);
+		ResponseEntity<List<String>> re = new ResponseEntity<>(list, HttpStatus.OK);
 		return re;
 	}
-	
-	
+
+	// 申請租借，接收表單
 	@PostMapping("/application/add")
-	public String processAddNewApplicationForm(Model model,@ModelAttribute("aplcBean") ApplicationBean ab, 
-		   BindingResult result	) {
+	public String processAddNewApplicationForm(Model model, @ModelAttribute("aplcBean") ApplicationBean ab,
+			BindingResult result) {
 		MemberBean memberBean = (MemberBean) model.getAttribute("memberBean");
 		int memberId = memberBean.getMemberId();
 		ab.setMemberId(memberId);
@@ -88,50 +87,52 @@ public class ApplicationController {
 		ab.setPayStatus(0);
 		String re = null;
 		try {
-			aplcService.addApplication(memberId,ab,ab.getDate(),ab.getTime());
-			return 	"redirect: " + ctx.getContextPath() + "/yourApplication";
-		}catch(Exception e) {
+			aplcService.addApplication(memberId, ab, ab.getDate(), ab.getTime());
+			return "redirect: " + ctx.getContextPath() + "/yourApplication";
+		} catch (Exception e) {
 			re = e.getMessage();
 			model.addAttribute("check", re);
 			return "application/failAddApplication";
 		}
 	}
 
-
+	// 申請租借，產生租借場地類別(客戶端)
 	@ModelAttribute("actClassList")
-	public Map<Integer, String> getActClassList(){
-		 Map<Integer, String> map = new HashMap<Integer, String>();
-		 List<ActClassBean>  list = aplcService.getAllActClass();
-		 for(ActClassBean ac : list) {
-			 map.put(ac.getActClassId(), ac.getName());
-		 }
+	public Map<Integer, String> getActClassList() {
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		List<ActClassBean> list = aplcService.getAllActClass();
+		for (ActClassBean ac : list) {
+			map.put(ac.getActClassId(), ac.getName());
+		}
 		return map;
 	}
+
+	// 申請租借，產生租借場地日期(可選期間為7天後開始至31天後)(客戶端)
 	@ModelAttribute("orderableDate")
-	public List<String> getorderableDate(){
+	public List<String> getorderableDate() {
 		java.util.Date current = new Date(System.currentTimeMillis());
-		//根據某個特定的時間 date (Date 型) 計算
+		// 根據某個特定的時間 date (Date 型) 計算
 		Calendar startDateC = Calendar.getInstance();
-		startDateC.setTime(current); //注意在此處將 specialDate 的值改為特定日期
-		startDateC.add(Calendar.DATE, 7); //特定時間的年後
+		startDateC.setTime(current); // 注意在此處將 specialDate 的值改為特定日期
+		startDateC.add(Calendar.DATE, 7); // 特定時間的年後
 		java.util.Date startDate = startDateC.getTime();
-		
+
 		Calendar endDateC = Calendar.getInstance();
-		endDateC.setTime(current); //注意在此處將 specialDate 的值改為特定日期
-		endDateC.add(Calendar.DATE, 31); //特定時間的年後
+		endDateC.setTime(current); // 注意在此處將 specialDate 的值改為特定日期
+		endDateC.add(Calendar.DATE, 31); // 特定時間的年後
 		java.util.Date endDate = endDateC.getTime();
-		
+
 		List<Date> orderableDates = aplcService.getOrderableDate(startDate, endDate);
 		List<String> orderableDatesStr = new ArrayList<String>();
-		for(Date orderableDate:orderableDates) {
+		for (Date orderableDate : orderableDates) {
 			orderableDatesStr.add(orderableDate.toString());
 		}
-		
+
 		return orderableDatesStr;
 	}
-	
+
 	@GetMapping("/yourApplication")
-	public String getYourApplication(Model model){
+	public String getYourApplication(Model model) {
 		MemberBean memberBean = (MemberBean) model.getAttribute("memberBean");
 		if (memberBean == null) {
 			return "redirect: " + ctx.getContextPath() + "/member/crm/login";
@@ -141,10 +142,9 @@ public class ApplicationController {
 		model.addAttribute("yourApplication", beans);
 		return "application/showYourApplication";
 	}
-	
+
 	@GetMapping("/allApplication")
-	public String getAllApplication(Model model,
-			@RequestParam(value ="pageNo",required = false)Integer pageNo){
+	public String getAllApplication(Model model, @RequestParam(value = "pageNo", required = false) Integer pageNo) {
 		MemberBean memberBean = (MemberBean) model.getAttribute("memberBean");
 		if (memberBean == null) {
 			return "redirect: " + ctx.getContextPath() + "/member/crm/login";
@@ -152,85 +152,86 @@ public class ApplicationController {
 		List<ApplicationBean> beans = aplcService.getAllAplcBean();
 		int totalPage = getTotalpage(beans.size());
 		model.addAttribute("totalPage", totalPage);
-		if(pageNo == null) {
+		if (pageNo == null) {
 			pageNo = 1;
-		};
+		}
+		;
 		int startRecordNo = (pageNo - 1) * 2;
 		int LastRecordNo = pageNo * 2;
-		if(LastRecordNo>beans.size()) {
+		if (LastRecordNo > beans.size()) {
 			LastRecordNo = beans.size();
 		}
-		List<ApplicationBean> beansPerPage = beans.subList(startRecordNo, LastRecordNo); 
+		List<ApplicationBean> beansPerPage = beans.subList(startRecordNo, LastRecordNo);
 		model.addAttribute("allApplication", beansPerPage);
 		return "application/showAllApplication";
 	}
-	
+
 	@GetMapping("/passApplication/{aplcId}")
-	public String passApplication(Model model,@PathVariable int aplcId) {
+	public String passApplication(Model model, @PathVariable int aplcId) {
 		aplcService.changeAplcBeanStatusById(aplcId, 2);
-		
+
 		return "redirect:/allApplication";
 	}
+
 	@GetMapping("/failApplication/{aplcId}")
-	public String failApplication(Model model,@PathVariable int aplcId) {
+	public String failApplication(Model model, @PathVariable int aplcId) {
 		aplcService.changeAplcBeanStatusById(aplcId, 3);
-		
+
 		return "redirect:/allApplication";
 	}
+
 	@GetMapping("/cancleApplication/{aplcId}")
-	public String cancleApplication(Model model,@PathVariable int aplcId) {
+	public String cancleApplication(Model model, @PathVariable int aplcId) {
 		aplcService.changeAplcBeanStatusById(aplcId, 4);
-		
-		return "redirect:/allApplication";
+
+		return "redirect:/yourApplication";
 	}
-	
+
 	@GetMapping("/changePayStatus/{aplcId}")
-	public String changePayStatus(Model model,@PathVariable int aplcId) {
-		
+	public String changePayStatus(Model model, @PathVariable int aplcId) {
+
 		aplcService.changeAplcBeanPayStatusById(aplcId, 1);
 		return "redirect:/allApplication";
 	}
-	
+
 	@GetMapping("/allApplicationByStatus")
-	public ResponseEntity<Map<String,Object>> getAllApplicationByStatus(Model model,@RequestParam(value = "statusId" ,required = false)Integer statusId
-			,@RequestParam(value ="pageNo",required = false)Integer pageNo){
-		
+	public ResponseEntity<Map<String, Object>> getAllApplicationByStatus(Model model,
+			@RequestParam(value = "statusId", required = false) Integer statusId,
+			@RequestParam(value = "pageNo", required = false) Integer pageNo) {
+
 		List<ApplicationBean> beans = null;
-		if(statusId == 0) {
+		if (statusId == 0) {
 			beans = aplcService.getAllAplcBean();
-		}else{
+		} else {
 			beans = aplcService.getAllAplcBeanByStatus(statusId);
 			System.out.println(beans);
 		}
 		System.out.println(beans);
 		int totalPage = getTotalpage(beans.size());
-		if(pageNo == null) {
+		if (pageNo == null) {
 			pageNo = 1;
-		};
+		}
+		;
 		int startRecordNo = (pageNo - 1) * 2;
 		int LastRecordNo = pageNo * 2;
-		if(LastRecordNo>beans.size()) {
+		if (LastRecordNo > beans.size()) {
 			LastRecordNo = beans.size();
 		}
-		List<ApplicationBean> beansPerPage = beans.subList(startRecordNo, LastRecordNo); 
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("selectedStatus",statusId);
-		map.put("application",  beansPerPage);
-		map.put("totalPage",totalPage);
-		map.put("pageNo",pageNo);
-		ResponseEntity<Map<String,Object>> re = new ResponseEntity<>(map,HttpStatus.OK);
+		List<ApplicationBean> beansPerPage = beans.subList(startRecordNo, LastRecordNo);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("selectedStatus", statusId);
+		map.put("application", beansPerPage);
+		map.put("totalPage", totalPage);
+		map.put("pageNo", pageNo);
+		ResponseEntity<Map<String, Object>> re = new ResponseEntity<>(map, HttpStatus.OK);
 		return re;
 	}
+
 	@GetMapping("/applicationDetail/{aplcId}")
-	public String getApplicationById(Model model,@PathVariable int aplcId) {
+	public String getApplicationById(Model model, @PathVariable int aplcId) {
 		ApplicationBean bean = aplcService.getAplcBeanId(aplcId);
 		model.addAttribute("application", bean);
 		return "application/applicationDetail";
 	}
-	
-	
 
-	
-	
-	
 }
